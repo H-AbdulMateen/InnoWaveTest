30dbdce5c044980b86a4806a019c6483ef7bfe17
H-AbdulMateen <abdulmateen6.ch@gmail.com>
2019-07-15 17:17:12 +0500

    Initial Commit

---
+++ app/src/main/java/com/abdulmateen/innowave/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.abdulmateen.innowave
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
