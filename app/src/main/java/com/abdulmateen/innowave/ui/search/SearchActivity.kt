3a59cf60dad732248c51e08d2a45d313af2f4c05
H-AbdulMateen <abdulmateen6.ch@gmail.com>
2019-07-15 17:46:19 +0500

    Architecture Setup

--- app/src/main/java/com/abdulmateen/innowave/ui/search/SearchActivity.kt
+++ app/src/main/java/com/abdulmateen/innowave/ui/search/SearchActivity.kt
@@ -1,12 +1,13 @@
-package com.abdulmateen.innowave
+package com.abdulmateen.innowave.ui.search
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.abdulmateen.innowave.R
 
-class MainActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_search)
     }
 }
